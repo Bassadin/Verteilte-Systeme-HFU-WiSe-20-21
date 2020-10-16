852797903efe0b1c0dbde892e8909fcbce1f94df
Bastian Hodapp <bastianhodapp@gmail.com>
2020-10-16 10:43:18 +0200

    working version
    no threading yet

--- src/blatt02/TimeService.java
+++ src/blatt02/TimeService.java
@@ -10,17 +10,34 @@ import java.net.Socket;
 
 public class TimeService {
 	static int port = 75;
-	
+
 	public static void main(String[] args) {
 		try {
 			System.out.println("Opening service on port " + port);
 			ServerSocket serverSocket = new ServerSocket(port);
 			Socket socket = serverSocket.accept();
-			
+
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			writer.write("Hallo Welt");
+
+			writer.write("time service");
+			writer.newLine();
+			writer.flush();
+
+			while (true) {
+				String commandString = reader.readLine();
+				if (commandString.equals("date")) {
+					writer.write(Clock.date());
+				} else if (commandString.equals("time")) {
+					writer.write(Clock.time());
+				} else {
+					socket.close();
+					break;
+				}
+				writer.newLine();
+				writer.flush();
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
