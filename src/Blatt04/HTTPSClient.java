1dacf5c15261b55d4f363dad5f59b0a1630a86b6
Bastian Hodapp <bastianhodapp@gmail.com>
2020-11-01 20:31:27 +0100

    getHTMLData method

--- src/Blatt04/HTTPSClient.java
+++ src/Blatt04/HTTPSClient.java
@@ -17,13 +17,13 @@ public class HTTPSClient {
 	static final int portNumber = 443;
 
 	public static void main(String[] args) {
-		System.out.println(urlExists("https://www.bundestag.de/presse"));
+		getHTMLDataOnly("https://www.bundestag.de/presse");
 	}
 
 	public static Socket getSocketForUrl(URL url) {
 		SocketFactory factory = SSLSocketFactory.getDefault();
-		try (Socket socket = factory.createSocket(url.getHost(), HTTPSClient.portNumber)) {
-			return socket;
+		try {
+			return factory.createSocket(url.getHost(), HTTPSClient.portNumber);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -67,6 +67,48 @@ public class HTTPSClient {
 
 	}
 
+	public static void getHTMLDataOnly(String url) {
+		try {
+			URL targetUrl = new URL(url);
+			try {
+				Socket socket = getSocketForUrl(targetUrl);
+				BufferedWriter printWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				printWriter.write(String.format("GET %s HTTP/1.1", targetUrl.getFile()));
+				printWriter.newLine();
+				printWriter.write(String.format("Host: %s", targetUrl.getHost()));
+				printWriter.newLine();
+				printWriter.newLine();
+
+				// Send message
+				printWriter.flush();
+
+				boolean contentStarted = false;
+				
+				// Receive HTTP answer
+				String nextLine = bufferedReader.readLine();
+				while (nextLine != null) {
+					if (!contentStarted && nextLine.contains("<!DOCTYPE")) {
+						contentStarted = true;
+					}
+					if (contentStarted) {
+						System.out.println(nextLine);
+					}
+					nextLine = bufferedReader.readLine();						
+				}
+				socket.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 	public static boolean urlExists(String url) {
 		try {
 			URL targetUrl = new URL(url);
